059c61a3caebe5e7690d54e090998478411de7ca
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-05-22 12:45:14 +0800

    Update cArray.c

--- cArray.c
+++ cArray.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-
-void bin(unsigned n)
-{
+void bin(unsigned n) {
 	unsigned i;
-	for (i = 1 << 31; i > 0; i = i / 2)
+	for (i = 1 << 31; i > 0; i = i / 2) {
 		(n & i) ? printf("1") : printf("0");
+	}
 	printf("\n");
 }
 
-int main()
-{
+int main() {
 	int arr[11] = {1,6,2,3,1,3,5,5,4,2,6};
 	int res = 0;
-	for (int i = 0; i < 11; i++)
-	{
+	for (int i = 0; i < 11; i++) {
 		bin(arr[i]);
 		bin(res);
 		res = res ^ arr[i];
