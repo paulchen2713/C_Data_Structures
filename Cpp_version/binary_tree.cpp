a16262f1ef131e2b382b43d5f7ad6d0722c5b516
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-08-17 15:33:44 +0800

    Update binary_tree.cpp

--- Cpp_version/binary_tree.cpp
+++ Cpp_version/binary_tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <time.h>
 
-using namespace std;
+// using namespace std;
 
 struct Node {
     int value;
@@ -9,104 +10,113 @@ struct Node {
 };
 
 class BinaryTree {
+private:
+    Node *root;
 
-    public:
-        BinaryTree() {
-            root = NULL;
-        };
+public:
+    BinaryTree() {
+        root = NULL;
+    };
 
-        void insert(int value) {
-            Node *p = new Node;
-            Node *parent;
-            
-            p->value = value;
-            p->left = NULL;
-            p->right = NULL;
-            parent = NULL;
-            if(is_empty()) {
-                root = p;
-            } else {
-                Node *ptr;
-                ptr = root;
-                while(ptr != NULL) {
-                    parent = ptr;
-                    if(value > ptr->value) {
-                        ptr = ptr->right;
-                    } else {
-                        ptr = ptr->left;
-                    }
-                }
-                if(value < parent->value) {
-                    parent->left = p;
-                } else {
-                    parent->right = p;
-                }
-            }
-        };
-
-        bool search(int key) {
-            return search(key, root);
-        };
-
-        bool search(int key, Node *ptr) {
-            if(ptr != NULL) {
-                if(key == ptr->value) {
-                    cout << "Found value!" << endl;
-                    return true;
-                }
-                cout << "Current node value is: " << ptr->value << endl;
-                if(key <= ptr->value) {
-                    cout << key << " is smaller! Going left!" << endl;
-                    return search(key, ptr->left);
-                } else {
-                    cout << key << " is larger! Going right!" << endl;
-                    return search(key, ptr->right);
-                }
-            } else {
-                cout << "Couldn't find element! Reached NULL!" << endl;
-                return false;
-            }
-        }
-
-        void displayTree() {
-            displayTree(root);
-        }
-
-        void displayTree(Node *ptr) {
-            if(ptr != NULL) {
-                displayTree(ptr->left);
-                cout << ptr->value << endl;
-                displayTree(ptr->right);
-            }
-        };
+    void insert(int value) {
+        Node *p = new Node;
+        Node *parent;
         
-        bool is_empty() {
-            return root == NULL;
+        p->value = value;
+        p->left = NULL;
+        p->right = NULL;
+        parent = NULL;
+        
+        if (is_empty()) {
+            root = p;
+        } 
+        else if (!is_empty()) {
+            Node *ptr;
+            ptr = root;
+            while(ptr != NULL) {
+                parent = ptr;
+                if (value > ptr->value) {
+                    ptr = ptr->right;
+                } 
+                else {
+                    ptr = ptr->left;
+                }
+            }
+            if (value < parent->value) {
+                parent->left = p;
+            } 
+            else {
+                parent->right = p;
+            }
         }
+    };
 
-        Node *get_root() {
-            return root;
+    bool search(int key) {
+        return search(key, root);
+    };
+
+    bool search(int key, Node *ptr) {
+        if (ptr != NULL) {
+            if (key == ptr->value) {
+                std::cout << "Found " << ptr->value << std::endl;
+                return true;
+            }
+            if (key <= ptr->value) {
+                return search(key, ptr->left);
+            } 
+            else {
+                return search(key, ptr->right);
+            }
+        } 
+        else if (ptr == NULL) {
+            std::cout << "Could not find " << key << std::endl;
+            return false;
         }
+        return NULL;
+    }
 
-    private:
+    void displayTree() {
+        displayTree(root);
+    }
 
-        Node *root;
+    void displayTree(Node *ptr) {
+        if (ptr != NULL) {
+            displayTree(ptr->left);
+            std::cout << ptr->value << std::endl;
+            displayTree(ptr->right);
+        }
+    };
+    
+    bool is_empty() {
+        return root == NULL;
+    }
 
+    Node *get_root() {
+        return root;
+    }
 };
 
 int main() {
 
     BinaryTree bin_tree;
-
-    bin_tree.insert(20);
-    bin_tree.insert(10);
-    bin_tree.insert(22);
-    bin_tree.insert(17);
-    bin_tree.insert(21);
-    bin_tree.insert(11);
+    
+    srand(time(NULL));
+    for (int i = 0; i < 24; i++) {
+        int rand_val = rand() % 100; // Generate number between 0 to 99
+        bin_tree.insert(rand_val);
+    }
+    
+    // bin_tree.insert(20);
+    // bin_tree.insert(10);
+    // bin_tree.insert(22);
+    // bin_tree.insert(17);
+    // bin_tree.insert(21);
+    // bin_tree.insert(11);
 
     bin_tree.displayTree();
-    cout << bin_tree.search(11) << endl;
+    
+    int val = rand() % 100;
+    bin_tree.search(val);
 
     return 0;
 }
