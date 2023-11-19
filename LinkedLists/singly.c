1fa205849ffa94dc5bebbc5f8b3538c3dcda6b6b
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2023-11-19 17:12:24 +0800

    Add files via upload

--- LinkedLists/singly.c
+++ LinkedLists/singly.c
@@ -9,8 +9,8 @@ typedef struct Node {
 
 void print_node(node_t* node) {
     printf("val:  %d\n", node->val);
-    printf("addr: %p\n", node);
-    printf("next: %p\n", node->next);
+    printf("addr: 0x%p\n", node);
+    printf("next: 0x%p\n", node->next);
 }
 
 node_t* create_new_node(int val) {
@@ -23,18 +23,6 @@ node_t* create_new_node(int val) {
     result->val = val;
     result->next = NULL;
     return result;
-}
-
-void freelist(node_t* head) {
-    if (head == NULL) return;
-    
-    node_t* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
-    free(head);
 }
 
 void printlist(node_t* head) {
@@ -51,15 +39,27 @@ void printlist(node_t* head) {
     printf("\n");
 }
 
-node_t* insert_at_head(node_t** head, node_t* node_to_insert) {
+void freelist(node_t* head) {
+    if (head == NULL) return;
+    
+    node_t* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+void insert_at_head(node_t** head, node_t* node_to_insert) {
+    if (*head == NULL) return;
+
     if (node_to_insert == NULL) {
         printf("try to insert NULL!\n");
-        return NULL;
+        return;
     }
     
     node_to_insert->next = *head;
     *head = node_to_insert;
-    return *head;
 }
 
 void insert_after_node(node_t* node_to_insert_after, node_t* newnode) {
@@ -90,6 +90,17 @@ node_t* find_node(node_t* head, int target) {
     }
     printf("no such node with value %d\n", target);
     return NULL;
+}
+
+node_t* middle_node(node_t* head) {
+    if (head == NULL) return NULL;
+
+    node_t* fast = head, *slow = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
 }
 
 void remove_node(node_t** head, node_t* node_to_remove) {
@@ -133,15 +144,9 @@ void remove_node2(node_t** head, int target) {
     remove_node(head, node_to_remove);
 }
 
-node_t* seplist(node_t* head) {
-    // 1. even odd 
-    
-    // 2. odd even 
-    
-    return NULL;
-}
-
 bool hasCycle(node_t* head) {
+    if (head == NULL || head->next == NULL) return false;
+
     node_t* fast = head, *slow = head;
     while (fast != NULL && fast->next != NULL) {
         fast = fast->next->next;
@@ -151,7 +156,22 @@ bool hasCycle(node_t* head) {
     return false;
 }
 
+void remove_elements(node_t** head, int target) {
+    if (*head == NULL) return;
+    
+    node_t* temp = *head;
+    while (temp->next != NULL) {
+        if (temp->next->val == target) {
+            temp->next = temp->next->next;
+        }
+        else temp = temp->next;
+    }
+
+    *head = ((*head)->val == target) ? (*head)->next : *head;
+}
+
 node_t* reverselist(node_t* head) {
+    if (head == NULL || head->next == NULL) return head;
     // 11 -> 12 -> 13
     // 13 -> 12 -> 11
     
@@ -159,16 +179,72 @@ node_t* reverselist(node_t* head) {
     while (head != NULL) {
         node_t* temp = head->next;
         head->next = newhead;
+
         newhead = head;
         head = temp;
     }
     return newhead;
 }
 
+void seplist(node_t** head, int mode) {
+    if (*head == NULL) return;
+    if (mode != 1 && mode != 2) {
+        printf("invalid mode index, please enter 1 or 2:\n");
+        printf("  1: even-odd list\n");
+        printf("  2: odd-even list\n");
+        return;
+    }
+
+    node_t* evenfirst = NULL, *evenlast = NULL;
+    node_t* oddfirst = NULL, *oddlast = NULL;
+
+    node_t* temp = *head;
+    int val = 0;
+    while (temp != NULL) {
+        val = temp->val;
+        if (val & 1) {  // odd
+            if (oddfirst == NULL) {
+                oddfirst = temp;
+                oddlast = oddfirst;
+            }
+            else {
+                oddlast->next = temp;
+                oddlast = oddlast->next;
+            }
+        }
+        else {
+            if (evenfirst == NULL) {
+                evenfirst = temp;
+                evenlast = evenfirst;
+            }
+            else {
+                evenlast->next = temp;
+                evenlast = evenlast->next;
+            }
+        }
+        temp = temp->next;
+    }
+    if (evenfirst == NULL || oddfirst == NULL) return;
+    
+    // 1. even->odd
+    if (mode == 1) {
+        evenlast->next = oddfirst;
+        oddlast->next = NULL;
+        *head = evenfirst;
+    }
+    // 2. odd->even
+    else if (mode == 2) {
+        oddlast->next = evenfirst;
+        evenlast->next = NULL;
+        *head = oddfirst;
+    }
+}
+
+
 int main() {
     const int Len = 25;
     
-    node_t* head = NULL;
+    node_t* head = create_new_node(33);
     node_t* temp = NULL;
     
     for (int i = 0; i < Len; i++) {
@@ -186,15 +262,50 @@ int main() {
     printlist(head);
     
     remove_node2(&head, 22);
+    insert_at_head(&head, create_new_node(99));
+    insert_at_head(&head, create_new_node(99));
+    insert_at_head(&head, create_new_node(99));
     printlist(head);
-    
-    temp = reverselist(head);
+
+    head = reverselist(head);
+    printlist(head);
+
+    remove_elements(&head, 99);
+    printlist(head);
+
+    seplist(&head, 1);
+    printlist(head);
+
+    temp = middle_node(head);
     printlist(temp);
+
+    seplist(&head, 2);
+    printlist(head);
+
+    temp = middle_node(head);
+    printlist(temp);
+
     
     freelist(head);
+    free(head);
     return 0;
 }
 
+/*
+D:\C++\LinkedLists>singly.exe
+found node with value 12
+24 - 23 - 22 - 21 - 20 - 19 - 18 - 17 - 16 - 15 - 14 - 13 - 12 - 11 - 10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1 - 0 - 33 -
+24 - 23 - 22 - 21 - 20 - 19 - 18 - 17 - 16 - 15 - 14 - 13 - 12 - 77 - 11 - 10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1 - 0 - 33 -
+24 - 23 - 22 - 21 - 20 - 19 - 18 - 17 - 16 - 15 - 14 - 13 - 77 - 11 - 10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1 - 0 - 33 -
+found node with value 22
+99 - 99 - 99 - 24 - 23 - 21 - 20 - 19 - 18 - 17 - 16 - 15 - 14 - 13 - 77 - 11 - 10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1 - 0 - 33 -
+33 - 0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10 - 11 - 77 - 13 - 14 - 15 - 16 - 17 - 18 - 19 - 20 - 21 - 23 - 24 - 99 - 99 - 99 -
+33 - 0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10 - 11 - 77 - 13 - 14 - 15 - 16 - 17 - 18 - 19 - 20 - 21 - 23 - 24 -
+0 - 2 - 4 - 6 - 8 - 10 - 14 - 16 - 18 - 20 - 24 - 33 - 1 - 3 - 5 - 7 - 9 - 11 - 77 - 13 - 15 - 17 - 19 - 21 - 23 -
+1 - 3 - 5 - 7 - 9 - 11 - 77 - 13 - 15 - 17 - 19 - 21 - 23 -
+33 - 1 - 3 - 5 - 7 - 9 - 11 - 77 - 13 - 15 - 17 - 19 - 21 - 23 - 0 - 2 - 4 - 6 - 8 - 10 - 14 - 16 - 18 - 20 - 24 -
+21 - 23 - 0 - 2 - 4 - 6 - 8 - 10 - 14 - 16 - 18 - 20 - 24 -
+*/
 
 
 
